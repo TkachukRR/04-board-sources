c6fe7d149232f8a6dea989999a0caa7905d9c158
TkachukRR <96269607+TkachukRR@users.noreply.github.com>
2022-03-11 10:20:55 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+const board = document.querySelector('#board')
+const colors = ['#620080', '370080', '#002d80', '#06a7b3', '#07ffff']
+const SQUARES_NUMBER = 700
+
+for (let i = 0; i < SQUARES_NUMBER; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+    square.addEventListener('mouseleave', () => removeColor(square))
+
+    board.append(square)
+}
+
+function setColor(element) {
+    const color = getRandomColor()
+    element.style.backgroundColor = color
+    element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+    element.style.backgroundColor = '#1d1d1d'
+    element.style.boxShadow = `0 0 2px #000`
+}
+
+function getRandomColor() {
+    const index = Math.floor(Math.random () * colors.length)
+    return colors[index]
+}
